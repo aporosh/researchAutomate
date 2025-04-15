516db14001cb946dee9e3a7c5d5f1297c875126c
aporosh <115151056+aporosh@users.noreply.github.com>
2025-04-15 22:11:56 +0300

    research date format

--- src/test/java/restApiTests/PetTests.java
+++ src/test/java/restApiTests/PetTests.java
@@ -13,8 +13,13 @@ import restApi.services.PetService;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 
@@ -24,7 +29,7 @@ public class PetTests {
     @Story("Create user")
     @Description("Проверяет отправку запроса на создлание пользователя и что в системе создался юзер с указанным именем")
     @Test
-    public void createPet() throws IOException {
+    public void createPet() throws IOException, ParseException {
 //        String[] photoUrls = {"string1", "string2"};
 //        ArrayList<String> photoUrlList = new ArrayList<>(Arrays.asList(photoUrls));
 //        Tag tag = new Tag(1, "test1");
@@ -47,5 +52,44 @@ public class PetTests {
                 .isNotNull();
 //                .extracting(PetData::getStatus)
 //                .isEqualTo(rq.getStatus());
+
+
+        SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date(1212121212121L);
+        System.out.println(formater.format(date));
+
+        long time = date.getTime();
+        System.out.println(time);
+
+        Date current = new Date(105, 5, 4, 12, 15, 0);
+        SimpleDateFormat formatter = new SimpleDateFormat("MMM-dd-YYYY");
+        String message = formatter.format(current);
+        System.out.println(message);
+
+        java.util.Date currentDate = new java.util.Date();
+        System.out.println(currentDate);
+
+        java.time.LocalDateTime currentDateTime = java.time.LocalDateTime.now();
+        System.out.println(currentDateTime);
+
+
+
+
+        String dateString = "2021-01-01";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date formatedDate = sdf.parse(dateString);
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(formatedDate);
+        c.add(Calendar.DATE, 1);  // number of days to add
+        formatedDate = c.getTime();
+
+        System.out.println(sdf.format(formatedDate)); // 2021-01-02
+
+
+        Date dateTime = new Date();
+        dateTime.setTime( Date.parse("Jul 06 12:15:00 2019") );
+
+
     }
 }
